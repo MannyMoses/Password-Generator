4cdc52fc6b3097380afd3f67989b49466ad5390f
Manny Moses <msm.prof@hotmail.com>
2020-05-11 17:12:15 -0500

    finished script

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # password-generator
 
+In this activity, we were given all the necessary code for a password generator. However we had to write the script in order for the generator to return a value for us based off of which character type the user input. 
+
 ## Acceptance Criteria
 
 ```
@@ -19,3 +21,4 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
+
--- script2.js
+++ script2.js
@@ -1,23 +1,7 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
 var length = Number(prompt("How many characters would you like your password to be? Enter a number between 8 and 128"));
 
 //ask for character type
-var charType = prompt("Enter a character type: lowercase, uppercase, special, numeric, all");
+var charType = prompt("Enter a character type:lowercase, uppercase, special, numeric");
 
 //generate password
 function generatePassword() {
@@ -32,8 +16,8 @@ function generatePassword() {
     charSet = "0123456789";
   } else if(charTypeLower === "special") {
     charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } else if(charTypeLower === "all") {
-      charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if(charTypeLower === "All") {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   }
   //returns the value
   var returnVal = "";
